8ffedfcdc90bab469e78ebae24f820f4a80095ea
Gayatri Hajar <0320200010@polman.astra.ac.id>
2021-05-26 22:03:10 +0700

    nambah setup wizard

---
+++ LatihanReport/Form3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace LatihanReport
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+
+            //      ambil dari app setting dan masukkan ke variable
+            String valueTestString = ConfigurationManager.AppSettings["TestString"];
+            int valueTestAngka = int.Parse(ConfigurationManager.AppSettings["TestAngka"]);
+
+            //menampilkan variable via message box
+            MessageBox.Show("Isi TestString : " + valueTestString);
+            MessageBox.Show("Isi TestAngka : " + valueTestAngka);
+        }
+    }
+}
